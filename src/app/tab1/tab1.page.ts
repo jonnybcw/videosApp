c35df3ce8aef414f04271ab403a86fbdf7e5b541
Jonny Eduardo Banach <jonnybanach@gmail.com>
2021-08-12 10:29:31 -0300

    Adicionado o array de filmes e a interface de filme

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,7 @@
+
 import { Component } from '@angular/core';
 import { AlertController, ToastController } from '@ionic/angular';
+import { IFilme } from '../models/IFilme.model';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,31 @@ import { AlertController, ToastController } from '@ionic/angular';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+
+  titulo = 'Vídeos App';
+
+  listaFilmes: IFilme[] = [
+    {
+      nome: 'Viúva Negra (2021)',
+      lancamento: '08/07/2021',
+      duracao: '2h 13m',
+      classificacao: 78,
+      cartaz: 'https://www.themoviedb.org/t/p/w1280/rKq1Vlw0Bqe2EEvdmIkkkgPQAGf.jpg',
+      generos: [
+        'Ação', 'Aventura', 'Thriller', 'Ficção científica'
+      ]
+    },
+    {
+      nome: 'O Esquadrão Suicida (2021)',
+      lancamento: '05/08/2021',
+      duracao: '2h 12m',
+      classificacao: 82,
+      cartaz: 'https://www.themoviedb.org/t/p/w1280/wTS3dS2DJiMFFgqKDz5fxMTri.jpg',
+      generos: [
+        'Ação', 'Aventura', 'Fantasia'
+      ]
+    }
+  ];
 
   constructor(public alertController: AlertController, public toastController: ToastController) {}
 
